de25b8af285f4769be6cb57a108d78298f996808
predeep <@chouhanpradeep720-ai>
2026-01-04 16:53:44 +0530

    node commit

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,15 @@
-FROM node:10 AS ui-build
-WORKDIR /usr/src/app
-COPY my-app/ ./my-app/
-RUN cd my-app && npm install && npm run build
+FROM node:18-alpine
 
-FROM node:10 AS server-build
-WORKDIR /root/
-COPY --from=ui-build /usr/src/app/my-app/build ./my-app/build
-COPY api/package*.json ./api/
-RUN cd api && npm install
-COPY api/server.js ./api/
+WORKDIR /home/app
 
-EXPOSE 3080
+COPY my-app ./my-app
+COPY api ./api
 
-CMD ["node", "./api/server.js"]
+WORKDIR /home/app/my-app
+RUN npm install
+RUN npm run build
+
+WORKDIR /home/app/api
+RUN npm install
+
+CMD ["node","server.js"] 
--- api/server.js
+++ api/server.js
@@ -3,24 +3,47 @@ const path = require('path');
 const app = express(),
       bodyParser = require("body-parser");
       port = 3080;
-
+const mongoose = require('mongoose');
 // place holder for the data
-const users = [];
+mongoose 
+.connect('mongodb://localhost:27017/my-db')
+.then(() => console.log('MongoDB connected'))
+.catch( err => console.error(err));
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../my-app/build')));
 
-app.get('/api/users', (req, res) => {
-  console.log('api/users called!')
-  res.json(users);
+const UserSchema = new mongoose.Schema({
+        name:String,
+        email:String
 });
 
-app.post('/api/user', (req, res) => {
-  const user = req.body.user;
-  console.log('Adding user:::::', user);
-  users.push(user);
-  res.json("user addedd");
+const User = mongoose.model('User', UserSchema);
+
+
+
+app.get('/api/users', async (req, res) => {
+    try {
+     const users = await User.find();
+     res.json(users);
+    } catch (err) {
+     res.status(500).json(err);
+    }
 });
+
+
+app.post('/api/user', async (req, res) =>{
+   try {
+      const user = new User(req.body);
+      await user.save();
+      res.json({ message: 'User added successfully'});
+   } catch (err) {
+     res.status(500).json(err);
+   }
+
+   });
+
+
 
 app.get('/', (req,res) => {
   res.sendFile(path.join(__dirname, '../my-app/build/index.html'));
